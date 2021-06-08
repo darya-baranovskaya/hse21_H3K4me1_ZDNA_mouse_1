a728f4f099314c6742c08192327769a84a8f44e0
Дарья Барановская <darya.baranovskaya01082001@gmail.com>
2021-06-08 19:14:32 +0300

    vizualize

---
+++ src/chip_seeker.R
@@ -0,0 +1,31 @@
+source('lib.R')
+library(ChIPseeker)
+#library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+library(TxDb.Mmusculus.UCSC.mm10.knownGene)
+library(clusterProfiler)
+
+###
+setwd("/Users/daryabaranovskaya/HSE_third_course/Bioinformatics/final_project/hse21_H3K4me1_ZDNA_mouse_1/src")
+
+
+#NAME <- 'DeepZ'
+NAME <- 'H3K4me1_MEL.ENCFF563AUO.mm10.filtered'
+#NAME <- 'H3K4me1_MEL.ENCFF687RAP.mm10.filtered'
+BED_FN <- paste0(DATA_DIR, NAME, '.bed')
+
+###
+
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+
+peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
+
+#pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.pdf'))
+png(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.png'))
+plotAnnoPie(peakAnno)
+dev.off()
+
+peak <- readPeakFile(BED_FN)
+pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
+covplot(peak, weightCol="V5")
+dev.off()
+ 

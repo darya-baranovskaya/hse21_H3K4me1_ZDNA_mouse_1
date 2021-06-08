1a2279e2f19a944118ef784340bdb44d66c6cc42
Дарья Барановская <darya.baranovskaya01082001@gmail.com>
2021-06-09 02:01:56 +0300

    cheap seeker

--- src/chip_seeker.R
+++ src/chip_seeker.R
@@ -8,9 +8,9 @@ library(clusterProfiler)
 setwd("/Users/daryabaranovskaya/HSE_third_course/Bioinformatics/final_project/hse21_H3K4me1_ZDNA_mouse_1/src")
 
 
-NAME <- 'H3K4me1_MEL.intersect_with_mouseZDNA1'
+#NAME <- 'H3K4me1_MEL.intersect_with_mouseZDNA1'
 #NAME <- 'mouseZ-DNA1.cut1-3'
-#NAME <- 'H3K4me1_MEL.ENCFF563AUO.mm10.filtered'
+NAME <- 'H3K4me1_MEL.ENCFF563AUO.mm10.filtered'
 #NAME <- 'H3K4me1_MEL.ENCFF687RAP.mm10.filtered'
 BED_FN <- paste0(DATA_DIR, NAME, '.bed')
 
@@ -25,8 +25,8 @@ png(paste0(OUT_DIR, 'chip_seeker.', NAME, '.plotAnnoPie.png'))
 plotAnnoPie(peakAnno)
 dev.off()
 
-peak <- readPeakFile(BED_FN)
-pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
-covplot(peak, weightCol="V5")
+#peak <- readPeakFile(BED_FN)
+#pdf(paste0(OUT_DIR, 'chip_seeker.', NAME, '.covplot.pdf'))
+#covplot(peak, weightCol="V5")
 #dev.off()
  

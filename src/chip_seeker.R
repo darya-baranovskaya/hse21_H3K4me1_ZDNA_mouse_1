7d851eba931e43f8d1267d141ed9f69c9e384b4e
Дарья Барановская <darya.baranovskaya01082001@gmail.com>
2021-06-09 13:49:28 +0300

    chenges

--- src/chip_seeker.R
+++ src/chip_seeker.R
@@ -9,14 +9,14 @@ setwd("/Users/daryabaranovskaya/HSE_third_course/Bioinformatics/final_project/hs
 
 
 #NAME <- 'H3K4me1_MEL.intersect_with_mouseZDNA1'
-#NAME <- 'mouseZ-DNA1.cut1-3'
-NAME <- 'H3K4me1_MEL.ENCFF563AUO.mm10.filtered'
+NAME <- 'mouseZ-DNA1.cut1-3'
+#NAME <- 'H3K4me1_MEL.ENCFF563AUO.mm10.filtered'
 #NAME <- 'H3K4me1_MEL.ENCFF687RAP.mm10.filtered'
 BED_FN <- paste0(DATA_DIR, NAME, '.bed')
 
 ###
 
-txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+txdb <- TxDb.Mmusculus.UCSC.mm10.knownGene
 
 peakAnno <- annotatePeak(BED_FN, tssRegion=c(-3000, 3000), TxDb=txdb, annoDb="org.Hs.eg.db")
 
--- src/lib.R
+++ src/lib.R
@@ -7,6 +7,6 @@ library(tibble)  # column_to_rownames
 ###
 setwd("/Users/daryabaranovskaya/HSE_third_course/Bioinformatics/final_project/hse21_H3K4me1_ZDNA_mouse_1/src")
 
-DATA_DIR <- 'data/'
+DATA_DIR <- '../data/'
 OUT_DIR <- '../images/'
 
